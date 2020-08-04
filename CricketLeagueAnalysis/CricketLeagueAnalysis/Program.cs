32392691a3480b4c9fc5f48c6442c5a901814701
mrunal93 <mrn26893@gmail.com>
2020-08-04 21:39:00 +0530

    [Mrunal]Add: Cricket League welcome messgae

---
+++ CricketLeagueAnalysis/CricketLeagueAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CricketLeagueAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cricket League Analysis Application");
+        }
+    }
+}
