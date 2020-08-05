c4c9522927d7ff5a316b208077f72f7de97dbfd9
mrunal93 <mrn26893@gmail.com>
2020-08-05 06:31:33 +0530

    [Mrunal]Add: create the function and short file with batting average

---
+++ CricketLeagueAnalysis/CricketLeagueAnalysis/CensusDAO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CricketLeagueAnalysis
+{
+    public class CensusDAO
+    {
+        public string POS { get; set; }
+        public string PLAYER { get; set; }
+        public string Mat { get; set; }
+        public string Inns { get; set; }
+        public string NO { get; set; }
+        public string Runs { get; set; }
+        public string HS { get; set; }
+        public string Avg { get; set; }
+        public string BF { get; set; }
+        public string SR { get; set; }
+        public string Hundreds { get; set; }
+        public string Fiftys { get; set; }
+        public string Fours { get; set; }
+        public string Sixs { get; set; }
+    }
+}
--- CricketLeagueAnalysis/CricketLeagueAnalysisNUnitTest/CricketLeagueAnalysisTest.cs
+++ CricketLeagueAnalysis/CricketLeagueAnalysisNUnitTest/CricketLeagueAnalysisTest.cs
@@ -1,18 +1,22 @@
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
+using System;
 
 namespace CricketLeagueAnalysisNUnitTest
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        string FILE_PATH_IPL_BATSMAN = @"C:\Users\Admin\Documents\CricketLeagueAnalysis\CricketLeagueAnalysis\CricketLeagueAnalysis\Resources\Day12 Data_01 IPL2019FactsheetMostRuns.csv";
+
 
         [Test]
-        public void Test1()
+        public void UCStateCodeDataPopulationDensity_WhenLoaded_ShouldReturnSortedResultByPopulationDensity()
         {
-            Assert.Pass();
+            CricketLeagueAnalysis.CricketLeagueCsv batttingAverage = new CricketLeagueAnalysis.CricketLeagueCsv(FILE_PATH_IPL_BATSMAN);
+            string battingAverageData = batttingAverage.BattingAverage();
+            JArray jArray = JArray.Parse(battingAverageData);
+            string firstValueFromCsv = jArray[0]["Avg"].ToString();
+            Assert.AreEqual("10.1", firstValueFromCsv);
         }
     }
 }
